52389f3efbb125f743af9cfcc2d89045b1b7678d
mohit.patel@extentia.com <mohit.patel@extentia.com>
2021-03-19 17:58:37 +0530

    error changes

--- src/test_case/user.integration.test.ts
+++ src/test_case/user.integration.test.ts
@@ -7,13 +7,13 @@ describe('checking middleware',()=>{
       
     });
     test('1. connected',async ()=>{
-        const result = await request(app.a).get('/');
+        const result = await request(app.app).get('/');
         expect(result.body).toEqual({message: 'SERVER CONNECTED'});
         expect(result.status).toEqual(200);
     });
 
     test('1. get all Users',async ()=>{
-        const result = await request(app).get('/employee');
+        const result = await request(app.app).get('/employee');
         expect(result.status).toEqual(200);
         expect(result.body).toStrictEqual(expect.anything())
     });
